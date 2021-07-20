fe060bbce5f7bee9ad01853c4d7009c500cc00e3
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-07-20 21:43:29 +0900

    input 값 list에 추가 및 삭제 기능 구현 및 li태그 삽입 및 개별 delete button 추가

--- js/list.js
+++ js/list.js
@@ -1,36 +1,61 @@
 // const addEl = document.querySelector('.to-do');
 // const delEl = document.querySelector('.to-do-del');
-const butEl = document.querySelectorAll('button')
+const butEl = document.querySelectorAll('body button')
 let putEl = document.getElementById("input");
-const orList = document.querySelector(".list ol")
+const orList = document.querySelector(".list ul")
 
 let el;
+let delBtn;
+let check = 0; // list에 내용 없음 확인
 
-// function list() {
-//   for(let i = 0; i < listEl.length; i++) {
-//     return listEl[i];
-//   }
-// }
-// list();
 function todoList() {
-
+  
 function add() {
+  
   if(putEl.value) {
     el = document.createElement('li');
+    // el.setAttribute('id', 'li');
+    el.classList.add('list-style');
+
+    delBtn = document.createElement('button');
+    delBtn.classList.add('btns');
+    delBtn.innerText = "Delete";
+    delBtn.setAttribute('id', 'dlt')
+    
     el.innerHTML = putEl.value;
+    el.appendChild(delBtn);
+
     orList.appendChild(el);
     // elText = document.createTextNode(display); //createTextNode는 값을 넣을때 사용
+
     console.log('추가되었습니다.')
     putEl.value = '';
-  } 
+
+    check++;
+    console.log(check);
+
+  } else if (!putEl.value) {
+      return console.log('값이 없습니다');
+    }
+  
+    delBtn.onclick = function() {
+        orList.removeChild(orList.querySelector('li:nth-child(n)'));
+    }
+  
 }
 
-function del() { 
-  // let liEl = document.querySelector('li');
-  orList.removeChild(orList.querySelector('li:last-child'));
-  console.log('삭제되었습니다.')
+function del() {
+  if(check > 0) {
+    orList.removeChild(orList.querySelector('li:last-child'));
+    check--;
+    console.log('삭제되었습니다.')
+    console.log(check);
+  } else if(check === 0) {
+    return console.log('값이 없습니다');
+  }
+}
 
-}; 
+
     
 //--------------------------------------------------
 // forEach문과 switch문으로 조건에 따른 결과값 실행
@@ -38,16 +63,15 @@ function del() {
 butEl.forEach(function(btn) {
   btn.addEventListener('click', () => {
     switch(btn.dataset.type) {
-      case 'add':
-        add()
-        break;
-      case 'del':
-        del()
-        break;
-      
-        // default:
-     }
-   })
+        case 'add':
+          add()
+          break;
+        case 'del':
+          del()
+          break;
+          // default:
+        }
+     })
   })
 }
 todoList();
