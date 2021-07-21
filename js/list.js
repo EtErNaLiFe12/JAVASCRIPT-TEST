e24fa5e0389793399b05c6785d90c256c0143eaf
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-07-21 20:35:02 +0900

    local storage 연결 및 값 저장/ 불러오기/삭제 기능 추가

--- js/list.js
+++ js/list.js
@@ -1,48 +1,93 @@
-// const addEl = document.querySelector('.to-do');
-// const delEl = document.querySelector('.to-do-del');
-const butEl = document.querySelectorAll('body button')
-let putEl = document.getElementById("input");
-const orList = document.querySelector(".list ul")
+const toDoForm = document.querySelector(".js-toDoForm");
+const putEl = toDoForm.querySelector("input");
+const butEl = document.querySelectorAll('button');
+const orList = document.querySelector(".list ul");
 
-let el;
-let delBtn;
 let check = 0; // list에 내용 없음 확인
 
-function todoList() {
-  
-function add() {
-  
-  if(putEl.value) {
-    el = document.createElement('li');
-    // el.setAttribute('id', 'li');
-    el.classList.add('list-style');
+const TODOS_LS = 'toDos';
 
-    delBtn = document.createElement('button');
-    delBtn.classList.add('btns');
+let toDos = [];
+
+//----------------------------------------------------------
+
+function dlt(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  orList.removeChild(li);
+  check--;
+  console.log(check);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+    toDos = cleanToDos
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+//----------------------------------------------------------
+
+function add(text) {
+    // el.setAttribute('id', newId);
+    const el = document.createElement('li');
+    const delBtn = document.createElement('button');
+    const span = document.createElement('span');
+    const newId = toDos.length + 1;
     delBtn.innerText = "Delete";
-    delBtn.setAttribute('id', 'dlt')
-    
-    el.innerHTML = putEl.value;
+    delBtn.addEventListener('click', dlt);
+    delBtn.classList.add('btns');
+    span.innerText = text;
+    el.appendChild(span);
     el.appendChild(delBtn);
-
+    el.id = newId;
+    el.classList.add('list-style');
     orList.appendChild(el);
     // elText = document.createTextNode(display); //createTextNode는 값을 넣을때 사용
 
     console.log('추가되었습니다.')
+ 
     putEl.value = '';
 
     check++;
     console.log(check);
+    
+    const toDoObj = {
+      text: text,
+      id: newId
+    };
 
-  } else if (!putEl.value) {
-      return console.log('값이 없습니다');
-    }
-  
-    delBtn.onclick = function() {
-        orList.removeChild(orList.querySelector('li:nth-child(n)'));
-    }
-  
+    toDos.push(toDoObj);
+    saveToDos();
 }
+
+//----------------------------------------------------------
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = putEl.value;
+  add(currentValue);
+  putEl.value = "";
+}
+function loadToDos() {
+ const loadedToDos = localStorage.getItem(TODOS_LS);
+ if(loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      add(toDo.text);
+   })
+ } 
+}
+function init() {
+  loadToDos();
+  toDoForm.addEventListener('submit', handleSubmit);
+}
+init();
+
+//----------------------------------------------------------
 
 function del() {
   if(check > 0) {
@@ -54,27 +99,24 @@ function del() {
     return console.log('값이 없습니다');
   }
 }
-
-
     
 //--------------------------------------------------
 // forEach문과 switch문으로 조건에 따른 결과값 실행
 // butEl.forEach(btn => {
-butEl.forEach(function(btn) {
-  btn.addEventListener('click', () => {
-    switch(btn.dataset.type) {
-        case 'add':
-          add()
-          break;
-        case 'del':
-          del()
-          break;
-          // default:
-        }
-     })
-  })
-}
-todoList();
+// butEl.forEach(function(btn) {
+//   btn.addEventListener('click', () => {
+//     switch(btn.dataset.type) {
+//         case 'add':
+//           add()
+//           break;
+//         case 'del':
+//           del()
+//           break;
+//           // default:
+//         }
+//      })
+//   })
+
 
 //---------------------------------------------------
 // for문과 switch문으로 조건에 따른 결과값 실행
